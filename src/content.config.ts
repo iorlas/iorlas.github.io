ce9bcde71ac195260dd85e6afaf999a2cfece0a7
Denis Tomilin <dt0xff@gmail.com>
2024-12-31 03:36:40 +0300

    feat: render

---
+++ src/content.config.ts
@@ -0,0 +1,33 @@
+import { defineCollection, z } from "astro:content";
+
+const BaseContent = z.object({
+  isDraft: z.boolean().default(false),
+  image: z.string().optional(),
+  title: z.string(),
+  excerpt: z.string().min(50).max(160),
+  publishDate: z
+    .string()
+    .optional()
+    .or(z.date())
+    .transform((str) => (str ? new Date(str) : undefined)),
+  updatedDate: z
+    .string()
+    .optional()
+    .or(z.date())
+    .transform((str) => (str ? new Date(str) : undefined)),
+});
+
+export const collections = {
+  blog: defineCollection({
+    type: "content",
+    schema: BaseContent.extend({}),
+  }),
+  kb: defineCollection({
+    type: "content",
+    schema: BaseContent.extend({}),
+  }),
+  page: defineCollection({
+    type: "content",
+    schema: BaseContent.extend({}),
+  }),
+};
